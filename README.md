3df10d41d9b242f77143589878c9558f1b7d6389
Carl-Erik Kopseng <carlerik@gmail.com>
2013-06-16 02:43:16 +0200

    Moved all files from old repo. Clean start

---
+++ README.md
@@ -0,0 +1,34 @@
+#Reverse engineering the Kindle web management api
+
+A utility library of functions that makes it possible to interact with
+Amazon's FIONA system for administrating the titles on your Kindle.
+Not yet expanded to a full program, just some examples on how to use it.
+
+## Sample output
+    Has 273 personal documents. The last one is Instapaper: Friday, Jun. 14
+    Has bought 68 e-book titles from Amazon. The last one is Kindle User's Guide
+    Last 15 personal documents:
+    b'[WQFCJDOWZTUYD67XG4RYNXDUVNZNPOJR]  Instapaper: Friday, Jun. 14'
+    b'[CASOR2TMKI2BJZZK3LCRIPSRPIYZSHQR]  Instapaper: Friday, May. 31'
+    b'[ECQRQWCYWWGJ3LEBNAVHI2RCYNWVETMD]  Instapaper: Tuesday, May. 28'
+    b'[G7XCGX3J7CIM6NPPVQYOE3T7LXMYD2JT]  Instapaper: Monday, May. 27'
+    b'[453JXHDZ54TNOYWXUYDK2KO726WU35DZ]  Where to Start | Trent Walton'
+    b'[UGA6O2GBB5AK7ARQKUIV6KIJBSQD6RUZ]  Instapaper: Monday, Apr. 29'
+    b'[ZNA3TVXIEKVLFOAUMKVOEIX2ODQYT4ND]  Kan man kode en fj\xc3\xb8l? -\nAftenposten'
+    b'[UNMNWZ4RMLLYNCTVCWSFP7LJE7OB7UWY]  Instapaper: Wednesday, Apr. 3'
+    b'[342EV2DYXSME5AQCSNEMH657VRWCNDJT]  Instapaper: Thursday, Mar. 14'
+    b'[O5ED7XM2HMRDYGFEBNZAPU4ZCV3VDNPY]  Instapaper: Saturday, Mar. 16'
+    b'[IS3MO5QBISEGJLELCD6E2WIJ5S2GGBVS]  Instapaper: Monday, Mar. 11'
+    b'[T3YSWNAEWN7BQINXN7QSLYPMEBRICL2Z]  Instapaper: Sunday, Mar. 10'
+    b'[EWOMLNB23DJL4JULE4NZH2WCT3KT6WP7]  Instapaper: Wednesday, Mar. 6'
+    b'[C3FSZOVACMBTPN5CPRXYPWFFJ6YWDPRX]  Instapaper: Tuesday, Mar. 5'
+    b'[64UKAMPFTLVAEAMWKJYZNTUEIUL335M3]  Instapaper: Thursday, Feb. 28'
+
+## Other
+There are also some notes (in Norwegian and English) I jotted down while trying to
+find out how Amazon's FIONA system for managing your Kindle archive is
+working, and how I was supposed to use curl to get in.
+
+Thankfully I abandened the idea of scripting this in Bash + cURL, and
+starting hacking on a Python version using the Requests library and
+BeautifulSoup. Some hours later I finally got in :-)
---
+++ kindle_manager.py
@@ -0,0 +1,80 @@
+import requests, re, sys, json
+from bs4 import BeautifulSoup
+from html.parser import HTMLParser
+
+#The session object that persists cookies and default values across requests
+s = requests.Session()
+h = HTMLParser();
+amazon = 'https://www.amazon.com'
+
+def usage():
+    print("USAGE:", sys.argv[0], " <email> <username> ")
+    sys.exit(1);
+
+def essential_info_string(item):
+    return '[' + item['asin'] + ']  ' + h.unescape(item['title'])
+
+def fetch_bought_titles(offset=0, count=15):
+    return fetch('queryOwnership_refactored2.html', offset, count)
+
+def fetch_personal_docs(offset=0, count=15):
+    return fetch('queryPdocs.html', offset, count)
+
+def delete_title(content_id):
+    r = s.post(amazon+'/gp/digital/fiona/du/fiona-delete.html', data = {'contentName':content_id})
+
+def fetch(action, offset, count):
+    r = s.post(amazon+'/gp/digital/fiona/manage/features/order-history/ajax/' + action,
+            data = {'offset':offset,'count':count} )
+    clean_json = remove_control_chars(r.text)
+    return json.loads(clean_json)['data']
+
+def remove_control_chars(json_string):
+    """remove control characters that are illegal json. Amazon does not remove them"""
+    return re.sub('[\x00-\x1f]', '',json_string)
+
+def init_session(email, password):
+    #we need to fake the user agent in order to get amazon to return anything other than 404
+    user_agent = "User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36" 
+    extra_headers = { 'User-Agent' : user_agent }
+    s.headers.update(extra_headers) 
+
+    r = s.get(amazon + '/gp/digital/fiona/manage')
+    soup = BeautifulSoup(r.content)
+    inputs = soup.find_all('input')
+
+    attributes = [i.attrs for i in inputs]
+    form_values = {}
+
+    for a in attributes:
+        form_values[a.get('name')] = a.get('value')
+    form_values.pop(None)
+
+    form_values['email'] = email
+    form_values['password'] = password
+
+    # This gets us in. The session cookies are crucial!
+    s.post( amazon + '/ap/signin',
+            data = form_values,
+            allow_redirects=True )
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        usage();
+
+    my_email = sys.argv[1]
+    my_password = sys.argv[2]
+
+    init_session(my_email, my_password)
+    docs = fetch_personal_docs()
+    books = fetch_bought_titles()
+
+    status_docs = "Has %s personal documents. The last one is %s"
+    status_books = "Has bought %s e-book titles from Amazon. The last one is %s"
+    print(status_docs % (docs['totalCount'], h.unescape(docs['items'][0]['title'])))
+    print(status_books % (books['totalCount'], h.unescape(books['items'][0]['title'])))
+    print("Last 15 personal documents:")
+    for doc in docs['items'][:15]:
+        print(essential_info_string(doc).encode('utf-8'))
+
+
